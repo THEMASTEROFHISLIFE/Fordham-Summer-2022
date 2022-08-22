9ae3ce96b742067106ce52c9b25ef3633b22ce76
GSAPP Kid <110209838+THEMASTEROFHISLIFE@users.noreply.github.com>
2022-08-21 20:17:11 -0400

    Add files via upload

---
+++ HW#1 Run.py
@@ -0,0 +1,28 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:/Users/nc281/AppData/Local/Programs/Python/Python310/HW#1.py ===
+Who was the creator of Python?
+
+==== RESTART: C:/Users/nc281/AppData/Local/Programs/Python/Python310/HW#1.py ===
+Who was the creator of Python?
+Guido van Rossum
+_______________________________________________
+When was Python created?
+Round about 1989, officially February 1991
+_______________________________________________
+In the Python community the person who created Python is commonly referred to as the “BDFL.” What does this mean?
+Benevolent dictator for life
+All Done!
+
+==== RESTART: C:/Users/nc281/AppData/Local/Programs/Python/Python310/HW#1.py ===
+Who was the creator of Python?
+Guido van Rossum
+_______________________________________________
+When was Python created?
+Round about 1989, officially February 1991
+_______________________________________________
+In the Python community the person who created Python is commonly referred to as the “BDFL.” What does this mean?
+Benevolent dictator for life
+______________________________________________
+All Done!
---
+++ HW#1.py
@@ -0,0 +1,20 @@
+#Nicholas Clarke
+#July 14, 2022
+# First Assignment of the course #StartingOutw/Python, by Tony Gaddis
+
+#HW_1 Page 29, #6 is p.70-71 via the ebook
+
+#Question 6:
+print("Who was the creator of Python?")
+print("Guido van Rossum")
+print("_______________________________________________")
+print("When was Python created?")
+print("Round about 1989, officially February 1991")
+print("_______________________________________________")
+print("In the Python community the person who created Python is commonly referred to as the “BDFL.” What does this mean?")
+print("Benevolent dictator for life")
+
+print("______________________________________________")
+print("All Done!")
+
+
